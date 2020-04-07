d5ca71d9e7aea60ed9ac3c381ea8260d60d8b508
Alexander K <as.kirienko@gmail.com>
2020-04-07 23:52:05 +0300

    search for console tool

--- YoutubeExtractor/YoutubeExtractor.App/Program.cs
+++ YoutubeExtractor/YoutubeExtractor.App/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace YoutubeExtractor.App
 {
@@ -8,32 +9,41 @@ namespace YoutubeExtractor.App
     {
         static void Main(string[] args)
         {
-            //try
-            //{
             AssemblySmartResolver.Install(typeof(Program).Assembly);
             Run(args);
+        }
 
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex);
-            //    return -1;
-            //}
-            
+        private static string Between(string source, string tag1, string tag2)
+        {
+            var n = source.IndexOf(tag1);
+            if (n == -1) return "";
+            var m = source.IndexOf(tag2, n + tag1.Length);
+            if (m == -1) return "";
+            return source.Substring(n + tag1.Length, m - n - tag1.Length);
+        }
+
+        private static bool HasAt<T>(T[] array, int index)
+        {
+            return index < array.Length;
+        }
+
+        private static T At<T>(T[] array, int index)
+        {
+            return index < array.Length ? array[index] : default(T);
         }
 
         private static void Run(string[] args)
         {
-            switch (args.ElementAtOrDefault(0))
+            switch (At(args, 0))
             {
-                case "download" when
-                    !string.IsNullOrWhiteSpace(args.ElementAtOrDefault(1)) &&
-                    !string.IsNullOrWhiteSpace(args.ElementAtOrDefault(2)):
-                    RunDownload(args[1], args[2], args.ElementAtOrDefault(3), args.ElementAtOrDefault(4), args.ElementAtOrDefault(5));
+                case "search" when HasAt(args, 1):
+                    RunSearch(args[1]);
                     break;
-                case "info" when
-                    !string.IsNullOrWhiteSpace(args.ElementAtOrDefault(1)):
+                case "info" when HasAt(args, 1):
                     RunInfo(args[1]);
+                    break;
+                case "download" when HasAt(args, 1) && HasAt(args, 2):
+                    RunDownload(args[1], args[2], At(args, 3), At(args, 4), At(args, 5));
                     break;
                 default:
                     Console.WriteLine("app.exe download https://www.youtube.com/watch?v=XXXXXXXX output.mp4  [0|480|720|1080] [Mp4|WebM] [Aac|Unknown]");
@@ -76,6 +86,33 @@ namespace YoutubeExtractor.App
 
             var videoDownloader = new VideoDownloader(video, path);
             videoDownloader.Execute();
+        }
+
+        private static void RunSearch(string search)
+        {
+            var query = string.Join("+", search.ToLowerInvariant().Split(' '));
+            var url = $"https://www.youtube.com/results?hl=en&search_query={WebUtility.UrlEncode(search)}";
+
+            using (var client = new WebClient())
+            {
+                client.Encoding = System.Text.Encoding.UTF8;
+                var str = client.DownloadString(url);
+
+                var items = str.Split(new string[] { "<a href=\"/watch?v=" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Skip(1).ToList();
+
+                foreach (var item in items)
+                {
+                    string id = Between(item, "", "\"");
+                    if (id.Length > 15)
+                        continue;
+
+                    string title = Between(item, "title=\"", "\"");
+                    string duration = Between(item, "> - Duration:", "<").Trim('.').PadLeft(8, ' ');
+                    string user = Between(item, "href=\"/user/", "\"");
+                    Console.WriteLine($"https://www.youtube.com/watch?v={id}     {duration}     [{user}]    '{title}'");
+                }
+            }
         }
     }
 }
